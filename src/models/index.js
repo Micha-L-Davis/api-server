16302ed51cd4b669f4727175ae5691e1b9175011
Micha Davis <m.leigh.davis@gmail.com>
2022-03-24 20:06:53 -0700

    added config for seqelize on heroku

--- src/models/index.js
+++ src/models/index.js
@@ -9,7 +9,15 @@ const DATABASE_URL = process.env.NODE_ENV === 'test' ?
   'sqlite::memory' :
   process.env.DATABASE_URL || 'postgresql://localhost:5432/api-db';
 
-const sequelize = new Sequelize(DATABASE_URL);
+const sequelize = new Sequelize(DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
+
 const SeriesModel = seriesSchema(sequelize, DataTypes);
 const GamesModel = gamesSchema(sequelize, DataTypes);
 
