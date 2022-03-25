477376776034962f49aec076c69261bbe2a02cea
Micha Davis <m.leigh.davis@gmail.com>
2022-03-24 17:57:11 -0700

    add primary routes

---
+++ routes/series.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const { seriesCollection } = require('../src/models');
+const express = require(express);
+const router = express.Router();
+
+router.post('/series', async (request, response, next) => {
+  let newSeriesData = request.body;
+
+  let responseData = await seriesCollection.create(newSeriesData);
+  response.send(responseData);
+});
+
+router.get('/series/:id', async (request, response, next) => {
+  let query = request.params;
+
+  let responseData = await seriesCollection.read(query);
+  response.send(responseData);
+});
+
+router.get('/series', async (request, response, next) => {
+  let responseData = await seriesCollection.readAll();
+  response.send(responseData);
+});
+
+router.put('/series/:id', async (request, response, next) => {
+  let updateData = request.body;
+  let query = request.params;
+
+  let responseData = await seriesCollection.update(updateData, query);
+  response.send(responseData);
+});
+
+router.delete('/series/:id', async (request, response, next) => {
+  let query = request.params;
+
+  let responseData = await seriesCollection.delete(query);
+  response.send(responseData);
+});
+
+module.exports = router;
